ef069cd190d6f8c0c1e9d312ea5753f8d27c1cb3
Seldom-SE <38388947+Seldom-SE@users.noreply.github.com>
2023-04-05 22:42:57 -0700

    Hunger

--- src/stats.rs
+++ src/stats.rs
@@ -1,3 +1,5 @@
+use std::f32::EPSILON;
+
 use bevy::prelude::*;
 use enum_map::{Enum, EnumMap};
 
@@ -51,6 +53,16 @@ impl Default for Health {
 
 #[derive(Component, Deref, DerefMut, Reflect, FromReflect)]
 #[reflect(Component)]
+pub struct Hunger(f32);
+
+impl Default for Hunger {
+    fn default() -> Self {
+        Hunger(1.0)
+    }
+}
+
+#[derive(Component, Deref, DerefMut, Reflect, FromReflect)]
+#[reflect(Component)]
 pub struct Radiation(f32);
 
 impl Default for Radiation {
@@ -68,11 +80,11 @@ pub struct RadiationSource {
 
 fn stat_propegation(
     mut commands: Commands,
-    mut query: Query<(Entity, &mut Stats, &mut Health, &mut Radiation)>,
+    mut query: Query<(Entity, &mut Stats, &mut Health, &Hunger, &mut Radiation)>,
     time: Res<Time>,
 ) {
     // Calculate stats for the frame
-    for (_, mut stats, mut health, mut radiation) in query.iter_mut() {
+    for (_, mut stats, mut health, _, mut radiation) in query.iter_mut() {
         let mut new_stats = stats.base_stats;
 
         let buffs = stats.buffs.clone();
@@ -97,11 +109,17 @@ fn stat_propegation(
         stats.current_stats = new_stats;
     }
     // Do radiation damage
-    for (_, stats, mut health, mut radiation) in query.iter_mut() {
+    for (_, stats, mut health, hunger, mut radiation) in query.iter_mut() {
         if radiation.0 > 0.8 {
-            health.0 -= stats.calc_damage(
-                stats.calc_radiation_damage((radiation.0 - 0.8) * time.delta_seconds()),
-            );
+            health.0 -= stats
+                .calc_damage(
+                    stats.calc_radiation_damage((radiation.0 - 0.8) * time.delta_seconds()),
+                )
+                .max(0.);
+        }
+
+        if hunger.0 <= EPSILON {
+            health.0 -= stats.calc_damage(time.delta_seconds());
         }
 
         if radiation.0 > 0.0 {
@@ -111,7 +129,7 @@ fn stat_propegation(
     }
 
     // Kill and adjust health.
-    for (entity, _, mut health, _) in query.iter_mut() {
+    for (entity, _, mut health, _, _) in query.iter_mut() {
         if health.0 <= 0.0 {
             commands.entity(entity).despawn();
         }
@@ -163,11 +181,15 @@ pub struct Buff {
 pub struct StatBundle {
     pub stats: Stats,
     pub health: Health,
+    pub hunger: Hunger,
     pub radiation: Radiation,
 }
 
 #[derive(Component)]
 struct HealthBar;
+
+#[derive(Component)]
+struct HungerBar;
 
 #[derive(Component)]
 struct RadiationBar;
@@ -176,7 +198,8 @@ pub fn stat_plugin(app: &mut App) {
     app.add_startup_system(init_ui)
         .add_system(stat_propegation)
         .add_system(update_ui)
-        .add_system(absorb_radiation);
+        .add_system(absorb_radiation)
+        .add_system(get_hungry);
 
     app.register_type::<Health>().register_type::<Radiation>();
 }
@@ -220,6 +243,18 @@ fn init_ui(mut commands: Commands, asset_server: Res<AssetServer>) {
 
                     parent.spawn((
                         TextBundle::from_section(
+                            "Hunger: 100",
+                            TextStyle {
+                                font: font.clone(),
+                                font_size: 40.0,
+                                color: Color::ORANGE,
+                            },
+                        ),
+                        HungerBar,
+                    ));
+
+                    parent.spawn((
+                        TextBundle::from_section(
                             "Radiation: 50",
                             TextStyle {
                                 font,
@@ -234,14 +269,18 @@ fn init_ui(mut commands: Commands, asset_server: Res<AssetServer>) {
 }
 
 fn update_ui(
-    mut healths: Query<&mut Text, (With<HealthBar>, Without<RadiationBar>)>,
-    mut radiations: Query<&mut Text, (With<RadiationBar>, Without<HealthBar>)>,
-    players: Query<(&Health, &Radiation), With<Player>>,
+    mut healths: Query<&mut Text, (With<HealthBar>, Without<HungerBar>, Without<RadiationBar>)>,
+    mut hungers: Query<&mut Text, (With<HungerBar>, Without<HealthBar>, Without<RadiationBar>)>,
+    mut radiations: Query<&mut Text, (With<RadiationBar>, Without<HealthBar>, Without<HungerBar>)>,
+    players: Query<(&Health, &Hunger, &Radiation), With<Player>>,
 ) {
-    let Ok((health, radiation)) = players.get_single() else { return };
+    let Ok((health, hunger, radiation)) = players.get_single() else { return };
 
     let mut health_text = healths.single_mut();
     health_text.sections[0].value = format!("Health: {}", (health.0 * 100.).ceil());
+
+    let mut hunger_text = hungers.single_mut();
+    hunger_text.sections[0].value = format!("Food: {}", (hunger.0 * 100.).ceil());
 
     let mut radiation_text = radiations.single_mut();
     radiation_text.sections[0].value = format!("Radiation: {}", (radiation.0 * 100.).ceil());
@@ -265,5 +304,14 @@ fn absorb_radiation(
                 **radiation = radiation.clamp(0., 1.);
             }
         }
+    }
+}
+
+const HUNGER_RATE: f32 = 0.005;
+
+fn get_hungry(mut hungers: Query<&mut Hunger>, time: Res<Time>) {
+    for mut hunger in hungers.iter_mut() {
+        **hunger -= HUNGER_RATE * time.delta_seconds();
+        **hunger = hunger.clamp(0., 1.);
     }
 }
